7370679cdb3a82b3f61126ff62155d60411096e7
Lucas Silva <lucssbarrosdasilva@gmail.com>
2022-03-23 19:38:50 -0300

    adicionando .env para conectar no banco

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,15 +2,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import io.javalin.Javalin;
 
 public class App {
-	public static String URL = "";
-	public static String NOME_BANCO = "";
-	public static String USUARIO = "";
-	public static String SENHA = "";
-
 	public static void main(String[] args) {
 		Javalin app = Javalin.create();
 		app.start(7070);
@@ -20,10 +15,18 @@ public class App {
 
 	public static String getAlunoById(String id) throws SQLException {
 		System.out.println("ID: " + id);
+		Dotenv dotenv = Dotenv.load();
 		Connection conexao = null;
 		try {
 			Class.forName("org.postgresql.Driver");
-			conexao = DriverManager.getConnection("jdbc:postgresql://" + URL + NOME_BANCO, USUARIO, SENHA);
+			conexao = DriverManager.getConnection(
+					"jdbc:postgresql://" + 
+					dotenv.get("URL") + 
+					dotenv.get("NOME_BANCO"), 
+					dotenv.get("USUARIO"), 
+					dotenv.get("SENHA")
+					);
+			
 			ResultSet aluno = conexao.createStatement().executeQuery("SELECT * FROM alunos WHERE id=" + id);
 			aluno.next();
 			return aluno.getString("nome");
